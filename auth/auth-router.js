7b5b9e8ff3ddc199f492db4ce4cbc5c5e2fffe3d
Shamskol <koladejun@yahoo.com>
2020-04-24 21:02:32 +0100

    Created jokes-router.spec.js to test thejokes-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -6,50 +6,48 @@ const { jwtSecret } = require("../config/secrets");
 
 const Users = require("../users/user-model");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
 
   Users.add(user)
-    .then(saved => {
+    .then((saved) => {
       res.status(201).json(saved);
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json(error);
     });
 });
 
-
-
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
-    .then(user => {
+    .then((user) => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = signToken(user); 
+        const token = signToken(user);
 
-        res.status(200).json({ token }); 
+        res.status(200).json({ token });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json(error);
     });
 });
 
 function signToken(user) {
   const payload = {
-    username: user.username
+    username: user.username,
   };
 
   const options = {
-    expiresIn: "1d"
+    expiresIn: "1d",
   };
 
   return jwt.sign(payload, jwtSecret, options);
